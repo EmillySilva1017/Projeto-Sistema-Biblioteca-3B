22e1b49cf22ac61ac8b8f9b4e55db7fb2104fec3
Jeffeson Macedo <antonio.macedo8@prof.ce.gov.br>
2026-03-23 09:37:07 -0300

    Estruturando em pastas

---
+++ sistema-biblioteca-3B/public/painel_adm.php
@@ -0,0 +1,15 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel Administrador</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="d-flex"> <?php include('menu.php'); ?></div>
+    <h1>Pagina em Manuteção</h1>
+</body>
+</html>
