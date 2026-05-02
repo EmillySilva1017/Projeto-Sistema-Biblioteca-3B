2bbc28978d3ead4ea73d72c4a7144f08cb2b0bc8
EmillyRodrigues1017 <emilly.silva287@aluno.ce.gov.br>
2026-05-02 18:28:58 -0300

    Desenvolvimento backend da tela de cadastro dos livros

--- livros/cadastro.php
+++ livros/cadastro.php
@@ -6,32 +6,36 @@ include('../includes/conexao.php');
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     //Verificação de campos vazios
-    if (empty($_POST['titulo']) || empty($_POST['autor']) || empty($_POST['isbn'])) {
+    if (empty($_POST['titulo']) || empty($_POST['autor']) || empty($_POST['n_registro'])) {
         $_SESSION['mensagem'] = "Preencha todos os campos!";
-        $_SESSION['tipo_mensagem'] = "danger";
         header('Location: cadastrar_livro.php');
         exit();
     }
     //Limpeza dos dados contra SQL Injection
     $titulo = mysqli_real_escape_string($conn, $_POST['titulo']);
     $autor = mysqli_real_escape_string($conn, $_POST['autor']);
-    $isbn = mysqli_real_escape_string($conn, $_POST['isbn']);
+    $editora = mysqli_real_escape_string($conn, $_POST['editora']);
+    $ano_aquisicao = mysqli_real_escape_string($conn, $_POST['ano_aquisicao']);
+    $genero = mysqli_real_escape_string($conn, $_POST['genero']);
+    $n_registro = mysqli_real_escape_string($conn, $_POST['n_registro']);
+    $cdd = mysqli_real_escape_string($conn, $_POST['cdd']);
+    $cdu = mysqli_real_escape_string($conn, $_POST['cdu']);
+    $selo = mysqli_real_escape_string($conn, $_POST['selo']);
 
-    // Verificação para ver se já existe algum livro com esse ISBN
-    $sqlCheck = "SELECT isbn FROM livros WHERE isbn = '$isbn'";
+    // Verificação para ver se já existe algum livro com esse número de registro
+    $sqlCheck = "SELECT numero_registro FROM livros WHERE numero_registro = '$n_registro'";
     $resCheck = mysqli_query($conn, $sqlCheck);
 
-    // Se o número de linhas retornadas for maior que 0, o ISBN já existe
+    // Se o número de linhas retornadas for maior que 0, o número de registro já existe
     if (mysqli_num_rows($resCheck) > 0) {
-        $_SESSION['mensagem'] = "Erro: Este ISBN já está cadastrado!";
-        $_SESSION['tipo_mensagem'] = "danger";
+        $_SESSION['mensagem'] = "Erro: Este N° de Registro já está cadastrado!";
         header('Location: cadastrar_livro.php');
         exit();
     }
 
     // Inserção no banco de dados
-    $sqlInserir = " INSERT INTO livros (titulo, autor, isbn)
-    VALUES ('$titulo', '$autor', '$isbn')";
+    $sqlInserir = " INSERT INTO livros (titulo_livro, autor, editora, ano_aquisicao, genero, numero_registro, cdd, cdu, selo)
+    VALUES ('$titulo', '$autor', '$editora', '$ano_aquisicao', '$genero', '$n_registro', '$cdd', '$cdu', '$selo')";
 
     //Executamos e verificamos se deu certo
     if (mysqli_query($conn, $sqlInserir)) {
